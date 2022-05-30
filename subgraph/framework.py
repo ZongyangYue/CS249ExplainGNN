5e4221dffdd43076151cbd6ef31b0cf880fcbe4d
baitingzbt <73772294+baitingzbt@users.noreply.github.com>
2022-05-29 22:54:32 -0700

    Add subgraph algorithm

--- subgraph/framework.py
+++ subgraph/framework.py
@@ -1,5 +1,7 @@
 #%%
 import os.path as osp
+from re import sub
+from sklearn import neighbors
 import tqdm
 import matplotlib.pyplot as plt
 import torch
@@ -18,24 +20,48 @@ from torch_geometric.nn import global_mean_pool
 from torch_geometric.utils import subgraph
 import numpy as np
 
-dataset = 'PROTEINS'
-path = osp.join(osp.dirname(osp.realpath("__file__")), '..', 'data', 'TUDataset')
-transform = T.Compose([T.GCNNorm(), T.NormalizeFeatures()])
-dataset = TUDataset(path, dataset, transform=transform)
-
 import networkx as nx
 from torch_geometric import utils
+from itertools import combinations
+
+def expand(starts, ends, target, max_depth=1, prev=[]):
+    node_neighbors = np.array([ends[idx] for idx, node in enumerate(starts) if node == target and ends[idx] != target])
+    prev.append(target)
+    if max_depth > 1:
+        for n in node_neighbors:
+            node_neighbors = np.concatenate((node_neighbors, expand(starts, ends, target=n, max_depth=max_depth-1, prev=prev)), axis=0)
+    indices = np.unique(node_neighbors, return_index=True)[1]
+    return np.array([node_neighbors[i] for i in sorted(indices)])
 
 
-# %%
-data = dataset[0]
-_subset = torch.from_numpy(np.array(list(range(20))))
-sub_data = data.subgraph(subset=_subset)
-nx.draw_networkx(utils.to_networkx(data, remove_self_loops=True))
+def process_one_graph(data, draw=False):
+    used = []
+    num_nodes = data.x.shape[0]
+    subgraph_sizes = [int(num_nodes/n) for n in range(2, 5)]
+    start_nodes, end_nodes = np.array(data.edge_index)
+    # make a grow from each node
+    for target_idx in range(num_nodes):
+        nodes_to_keep = expand(starts=start_nodes, ends=end_nodes, target=target_idx, max_depth=3, prev=[])
+        for size in subgraph_sizes:
+            # select the grows based on size
+            _subset = nodes_to_keep[:size]
+            # remove repetitive stuff
+            if list(set(_subset)) in used:
+                break
+            else:
+                used.append(list(set(_subset)))
+            _subset = torch.from_numpy(np.array(_subset))
+            sub_data = data.subgraph(subset=_subset)
+            if draw:
+                nx.draw_networkx(utils.to_networkx(sub_data, remove_self_loops=True))
+                plt.show()
 
 
-# %%
-nx.draw_networkx(utils.to_networkx(sub_data, remove_self_loops=True))
+if __name__ == "__main__":
+    dataset = 'PROTEINS'
+    path = osp.join(osp.dirname(osp.realpath("__file__")), '..', 'data', 'TUDataset')
+    transform = T.Compose([T.GCNNorm(), T.NormalizeFeatures()])
+    dataset = TUDataset(path, dataset, transform=transform)
 
-
-# %%
+    for data in dataset:
+        process_one_graph(data, draw=True)
