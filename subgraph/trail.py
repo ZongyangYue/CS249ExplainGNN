df8e8178f773b94d924f077653ad81145ec9f32e
baitingzbt <73772294+baitingzbt@users.noreply.github.com>
2022-05-29 22:55:01 -0700

    Delete trail.py

--- subgraph/trail.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-G = nx.Graph()
-import torch
-print(torch.__version__)
-from torch_geometric.datasets import TUDataset
-dataset = TUDataset(root = "datas", name="PROTEINS")
-print(G)
